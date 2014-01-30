e4eb57b504abcea106f6acb5d670702916337e4c
David Lohmeyer <dlohmeyer@acton.org>
2014-01-30 14:21:36 -0500

    Separated license file, added --opt flag to dumps to make importing quicker on large DB's

--- db.dump.sh
+++ db.dump.sh
@@ -5,7 +5,7 @@
 # vilepickle@gmail.com
 # Vilepickle.com
 # 1/4/13
-# Version 1.0
+# Version 1.1 (1/30/2014)
 #
 #############################################################################################
 #
@@ -13,27 +13,6 @@
 # Configure the variables below.
 # Place this in a directory where you want your database dump to be output and execute
 # the script.
-#
-#############################################################################################
-#
-# Copyright (c) 2013 David Lohmeyer
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights to 
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
-# the Software, and to permit persons to whom the Software is furnished to do so, 
-# subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all 
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 #############################################################################################
 
@@ -117,5 +96,5 @@ if [ $COMPRESS = 1 ]
 		gzip -9 --force $EXTRACTEDFILE > $DBFILENAME
 	else
 		# Dump the database
-		mysqldump -u $USER -p$PW -h $SERVER $DATABASE --result-file=$DBFILENAME
+		mysqldump --opt -u $USER -p$PW -h $SERVER $DATABASE --result-file=$DBFILENAME
 fi
